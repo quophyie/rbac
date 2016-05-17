444d57d60817716633e06736694bc4a0f44a84d8
andrezzoid <andre.jonas@gmail.com>
2016-05-17 09:42:34 +0100

    Changed allow method to authorize. Version bump.

--- test/index.js
+++ test/index.js
@@ -44,22 +44,22 @@ describe('RBAC', function () {
 
     nock('http://www.example.com', opts)
       .post('/authorize', {
-        user: 1,
+        userId: 1,
         permission: 'users:create'
       })
       .reply(401)
       .post('/authorize', {
-        user: 1,
+        userId: 1,
         permission: 'users:read'
       })
       .reply(200)
       .post('/authorize', {
-        user: 1,
+        userId: 1,
         permission: 'users:create'
       })
       .reply(401)
       .post('/authorize', {
-        user: 1,
+        userId: 1,
         permission: 'users:read'
       })
       .reply(200)
@@ -73,34 +73,34 @@ describe('RBAC', function () {
     expect(() => new Rbac({ remoteAuth: {} })).to.throw(TypeError)
   })
 
-  it('Rbac.allow should fail if user isn\'t allowed the existing permission locally', function (done) {
+  it('Rbac.authorize should fail if user isn\'t allowed the existing permission locally', function (done) {
     const rbac = new Rbac({ checkPermission: checkPermission })
     rbac
-      .allow(1, 'users:create')
-      .then(() => Code.fail('Rbac.allow should fail'))
+      .authorize(1, 'users:create')
+      .then(() => Code.fail('Rbac.authorize should fail'))
       .catch((err) => {
         expect(err).to.be.an.error('Inexistent User or Permission')
         done()
       })
   })
 
-  it('Rbac.allow should pass if user is allowed the existing permission locally', function (done) {
+  it('Rbac.authorize should pass if user is allowed the existing permission locally', function (done) {
     const rbac = new Rbac({ checkPermission: checkPermission })
     rbac
-      .allow(1, 'users:read')
+      .authorize(1, 'users:read')
       .then(done)
       .catch(done)
   })
 
-  it('Rbac.allow should pass if user is allowed any of the existing permissions locally', function (done) {
+  it('Rbac.authorize should pass if user is allowed any of the existing permissions locally', function (done) {
     const rbac = new Rbac({ checkPermission: checkPermission })
     rbac
-      .allow(1, ['users:read', 'users:create'])
+      .authorize(1, ['users:read', 'users:create'])
       .then(done)
       .catch(done)
   })
 
-  it('Rbac.allow should fail if user isn\'t allowed the existing permission remotely', function (done) {
+  it('Rbac.authorize should fail if user isn\'t allowed the existing permission remotely', function (done) {
     const rbac = new Rbac({
       remoteAuth: {
         url: 'http://www.example.com/authorize',
@@ -110,15 +110,15 @@ describe('RBAC', function () {
       }
     })
     rbac
-      .allow(1, 'users:create')
-      .then(() => Code.fail('Rbac.allow should fail'))
+      .authorize(1, 'users:create')
+      .then(() => Code.fail('Rbac.authorize should fail'))
       .catch((err) => {
         expect(err.statusCode).to.equal(401)
         done()
       })
   })
 
-  it('Rbac.allow should pass if user is allowed the existing permission remotely', function (done) {
+  it('Rbac.authorize should pass if user is allowed the existing permission remotely', function (done) {
     const rbac = new Rbac({
       remoteAuth: {
         url: 'http://www.example.com/authorize',
@@ -128,12 +128,12 @@ describe('RBAC', function () {
       }
     })
     rbac
-      .allow(1, 'users:read')
+      .authorize(1, 'users:read')
       .then(done)
       .catch(done)
   })
 
-  it('Rbac.express.allow should fail if user isn\'t allowed the existing permission remotely', function (done) {
+  it('Rbac.express.authorize should fail if user isn\'t allowed the existing permission remotely', function (done) {
     const rbac = new Rbac({
       remoteAuth: {
         url: 'http://www.example.com/authorize'
@@ -141,7 +141,7 @@ describe('RBAC', function () {
     })
     const middleware = rbac
       .express
-      .allow('users:create')
+      .authorize('users:create')
 
     const req = {
       user: { id: 1 },
@@ -158,7 +158,7 @@ describe('RBAC', function () {
     })
   })
 
-  it('Rbac.express.allow should pass if user is allowed the existing permission remotely', function (done) {
+  it('Rbac.express.authorize should pass if user is allowed the existing permission remotely', function (done) {
     const rbac = new Rbac({
       remoteAuth: {
         url: 'http://www.example.com/authorize'
@@ -166,7 +166,7 @@ describe('RBAC', function () {
     })
     const middleware = rbac
       .express
-      .allow('users:read')
+      .authorize('users:read')
 
     const req = {
       user: { id: 1 },
@@ -183,11 +183,11 @@ describe('RBAC', function () {
     })
   })
 
-  it('Rbac.express.allow should fail if user isn\'t allowed the existing permission locally', function (done) {
+  it('Rbac.express.authorize should fail if user isn\'t allowed the existing permission locally', function (done) {
     const rbac = new Rbac({ checkPermission: checkPermission })
     const middleware = rbac
       .express
-      .allow('users:create')
+      .authorize('users:create')
 
     const req = {
       user: { id: 1 },
@@ -204,11 +204,11 @@ describe('RBAC', function () {
     })
   })
 
-  it('Rbac.express.allow should pass if user is allowed the existing permission locally', function (done) {
+  it('Rbac.express.authorize should pass if user is allowed the existing permission locally', function (done) {
     const rbac = new Rbac({ checkPermission: checkPermission })
     const middleware = rbac
       .express
-      .allow('users:read')
+      .authorize('users:read')
 
     const req = {
       user: { id: 1 },
